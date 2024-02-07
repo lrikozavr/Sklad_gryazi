4741cf36bfbeaf2b440b0734f0d36e01bc1b75e9
lrikozavr <sorokakiril@gmail.com>
2024-02-07 07:06:29 +0200

    upd

--- gui/main_window.py
+++ gui/main_window.py
@@ -3,19 +3,21 @@ from PySide6.QtGui import (QAction, QCursor, QDesktopServices, QGuiApplication, 
                            QStandardItemModel)
 from PySide6.QtWidgets import (QMainWindow, QToolBar, QListWidget, QListWidgetItem,
                                QPushButton, QMessageBox, QFileDialog, QProgressBar,
-                               QApplication, QCheckBox, QComboBox,
-                               QCommandLinkButton, QDateTimeEdit, QDial,
+                               QApplication, QCheckBox, QComboBox, QCommonStyle,
+                               QCommandLinkButton, QDateEdit, QDial,
                                QDialog, QDialogButtonBox, QFileSystemModel,
                                QGridLayout, QGroupBox, QHBoxLayout, QLabel,
                                QLineEdit, QListView, QMenu, QPlainTextEdit,
-                               QProgressBar, QPushButton, QRadioButton,
-                               QScrollBar, QSizePolicy, QSlider, QSpinBox,
+                               QProgressBar, QPushButton, QRadioButton, QTimeEdit,
+                               QScrollBar, QSizePolicy, QSlider, QSpinBox, QStyle,
                                QStyleFactory, QTableWidget, QTabWidget, QMenuBar,
                                QTextBrowser, QTextEdit, QToolBox, QToolButton,
                                QTreeView, QVBoxLayout, QWidget, QTableWidgetItem, QLayout)
 
 from PySide6.QtAxContainer import QAxSelect, QAxWidget
-from PySide6.QtCore import Qt, QSize, QFile, QDir, QTextStream, QStringConverter, QTimer, Slot
+from PySide6.QtCore import Qt, QSize, QFile, QDir, QTextStream, QStringConverter, QTimer, Slot, QDate, QTime
+
+import time
 
 def class_name(o):
     return o.metaObject().className()
@@ -31,7 +33,7 @@ def embed_into_hbox_layout(w, margin=5):
     result = QWidget()
     layout = QHBoxLayout(result)
     layout.setContentsMargins(margin, margin, margin, margin)
-    layout.addWidget(w)
+    layout.addLayout(w)
     return result
 
 class ProgressBar(QDialog):
@@ -77,6 +79,11 @@ def open_file(widget):
         return
     return file_name
 
+def write_to_log(log,text):
+    temp_text = log.toPlainText()
+    temp_text += f"\n[{QTime.currentTime().toPython()}]: {text}"
+    log.setText(temp_text)
+    
 
 
 class MainWindow(QMainWindow):
@@ -107,29 +114,13 @@ class MainWindow(QMainWindow):
         self.axWidget = QAxWidget()
         #self.setCentralWidget(self.axWidget)
         
-
-        self.list = QListWidget()
-        #self.setCentralWidget(self.list)
-        for i in range(10):
-            item = QListWidgetItem(f"Item {i}")
-            item.setTextAlignment(Qt.AlignCenter)
-            self.list.addItem(item)
-
-        text_edit = QTextEdit("rich_text")        
-
-        button = QPushButton("Open File", self)
-        button.clicked.connect(self.handleButton)
-
         itemview_tabwidget = self.create_itemview_tabwidget()
 
-        top_layout = QHBoxLayout()
-        top_layout.addWidget(self.list)
-
         main_layout = QGridLayout()
-        main_layout.addLayout(top_layout, 0, 11, 7, 5)
-        main_layout.addWidget(itemview_tabwidget, 0, 0, 7, 7)
-        main_layout.addWidget(text_edit,7,0,2,16)
-        main_layout.addWidget(button,0,7,4,4)
+        #main_layout.addLayout(top_layout, 0, 11, 7, 5)
+        main_layout.addWidget(itemview_tabwidget, 0, 0, 7, 16)
+        #main_layout.addWidget(text_edit,7,0,2,16)
+        #main_layout.addWidget(button,0,7,4,4)
         #list.resize()
         
         main_widget = QWidget()
@@ -138,20 +129,17 @@ class MainWindow(QMainWindow):
         #self.button = QPushButton("My Button", self)
         #self.button.clicked.connect(self.handleButton)
         #
-    
-    def create_video_view_layout(self):
-        result = QHBoxLayout()
-        init_widget(result, "vide_view_layout")
-        
-
-        return
+        write_to_log(self._observation_log,"Start!")
+        write_to_log(self._observation_log,"Hear we go!")
+        #time.sleep(0.00001)
+        write_to_log(self._observation_log,"Bruh")
 
 
     def create_itemview_tabwidget(self):
         result = QTabWidget()
         init_widget(result, "bottomLeftTabWidget")
         result.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
-
+        '''
         tree_view = QTreeView()
         init_widget(tree_view, "treeView")
         filesystem_model = QFileSystemModel(tree_view)
@@ -183,8 +171,179 @@ class MainWindow(QMainWindow):
         result.addTab(embed_into_hbox_layout(list_view), "List")
         result.addTab(embed_into_hbox_layout(icon_mode_listview),
                       "Icon Mode List")
+        '''
+        video_layout = self.create_video_layout()
+        result.addTab(embed_into_hbox_layout(video_layout), "Camera")
         return result
     
+    def create_video_layout(self):
+        result = QGridLayout()
+
+        #X
+        X_label = QLabel("X:\t")
+        self.X_value_label = QLabel("_")
+        #Y
+        Y_label = QLabel("Y:\t")
+        self.Y_value_label = QLabel("_")
+        #
+        xy_text_layout = QHBoxLayout()
+        xy_text_layout.addWidget(X_label,1)
+        xy_text_layout.addWidget(self.X_value_label,3)
+        xy_text_layout.addWidget(Y_label,1)
+        xy_text_layout.addWidget(self.Y_value_label,3)
+        
+        #Az
+        Az_label = QLabel("Az:\t")
+        self.Az_value_label = QLabel("_")
+        #H
+        H_label = QLabel("H:\t")
+        self.H_value_label = QLabel("_")
+        #
+        AzH_text_layout = QHBoxLayout()
+        AzH_text_layout.addWidget(Az_label,1)
+        AzH_text_layout.addWidget(self.Az_value_label,3)
+        AzH_text_layout.addWidget(H_label,1)
+        AzH_text_layout.addWidget(self.H_value_label,3)
+
+        #dAz
+        dAz_label = QLabel("dAz:\t")
+        self.dAz_value_label = QLabel("_")
+        #dh
+        dH_label = QLabel("dH:\t")
+        self.dH_value_label = QLabel("_")
+        #
+        dAzH_text_layout = QHBoxLayout()
+        dAzH_text_layout.addWidget(dAz_label,1)
+        dAzH_text_layout.addWidget(self.dAz_value_label,3)
+        dAzH_text_layout.addWidget(dH_label,1)
+        dAzH_text_layout.addWidget(self.dH_value_label,3)
+        
+        #CurrentTimeValue
+        self.CurrentTimeValueEdit = QTextBrowser()
+        self.CurrentTimeValueEdit.setText(str(QTime.currentTime().toPython()))
+        self.CurrentTimeValueEdit.setFixedSize(QSize(93,28))
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.updateCurrentTime)
+        timer.start(100)
+
+        #ArrowButtonBlock
+        style = QStyleFactory.create("Fusion")
+        left_arrow_button = QPushButton()
+        left_arrow_button.setIcon(style.standardIcon(style.StandardPixmap(52)))
+        left_arrow_button.clicked.connect(self.left_arrow_button)
+
+        right_arrow_button = QPushButton()
+        right_arrow_button.setIcon(style.standardIcon(style.StandardPixmap(53)))
+        right_arrow_button.clicked.connect(self.right_arrow_button)
+        
+        up_arrow_button = QPushButton()
+        up_arrow_button.setIcon(style.standardIcon(style.StandardPixmap(50)))
+        up_arrow_button.clicked.connect(self.up_arrow_button)
+
+        down_arrow_button = QPushButton()
+        down_arrow_button.setIcon(style.standardIcon(style.StandardPixmap(51)))
+        down_arrow_button.clicked.connect(self.down_arrow_button)
+        
+        arrow_buttons_layout = QGridLayout()
+        arrow_buttons_layout.addWidget(left_arrow_button, 4, 0, 4, 4)
+        arrow_buttons_layout.addWidget(right_arrow_button, 4, 8, 4, 4)
+        arrow_buttons_layout.addWidget(up_arrow_button, 0, 4, 4, 4)
+        arrow_buttons_layout.addWidget(down_arrow_button, 8, 4, 4, 4)
+
+        #Start
+        start_button = QPushButton("Start")
+        start_button.clicked.connect(self.camera_start)
+
+        #Stop
+        stop_button = QPushButton("Stop")
+        stop_button.clicked.connect(self.camera_stop)
+
+        #Continue/Next
+        continue_button = QPushButton("Continue/Next")
+        continue_button.clicked.connect(self.camera_continue)
+
+        #view CheckBox: grid, star, trajectory
+        self.camera_grid_check_box = QCheckBox("add Grid")
+        self.camera_grid_check_box.setChecked(False)
+        self.camera_grid_check_box.clicked.connect(self.paint_camera_grid)
+
+        self.camera_star_check_box = QCheckBox("add Star")
+        self.camera_star_check_box.setChecked(False)
+        self.camera_star_check_box.clicked.connect(self.paint_camera_star)
+        
+        self.camera_trajectory_check_box = QCheckBox("add Trajectory")
+        self.camera_trajectory_check_box.setChecked(False)
+        self.camera_trajectory_check_box.clicked.connect(self.paint_camera_trajectory)
+
+        buttons_layout = QVBoxLayout()
+        buttons_layout.addLayout(xy_text_layout, 1)
+        buttons_layout.addLayout(AzH_text_layout, 1)
+        buttons_layout.addLayout(dAzH_text_layout, 1)
+        buttons_layout.addWidget(self.CurrentTimeValueEdit, 1)
+        buttons_layout.addLayout(arrow_buttons_layout, 3)
+        buttons_layout.addWidget(start_button, 1)
+        buttons_layout.addWidget(stop_button, 1)
+        buttons_layout.addWidget(continue_button, 1)
+        buttons_layout.addWidget(self.camera_grid_check_box, 1)
+        buttons_layout.addWidget(self.camera_star_check_box, 1)
+        buttons_layout.addWidget(self.camera_trajectory_check_box, 1)
+        
+        #Вивід роботи модулів
+        self._observation_log = QTextBrowser()
+        init_widget(self._observation_log, "observation_error_logTextBrowser")
+
+        self.table_list_observations = QTableWidget()
+        init_widget(self.table_list_observations, "table_list_observationsTableWidget")
+
+        #button.clicked.connect(self.handleButton)
+        self.camera_view = QWidget()
+        init_widget(self.camera_view, "camera_view")
+        
+        result.addWidget(self.camera_view, 0, 0, 7, 7)
+        #Треба додати сюди кнопки керування
+        #result.addStretch(2)
+        result.addLayout(buttons_layout, 0, 7, 7, 3)
+        result.addWidget(self.table_list_observations, 0, 10, 7, 6)
+        result.addWidget(self._observation_log, 7, 0, 2, 16)
+
+        return result
+    
+    def left_arrow_button(self):
+        pass
+
+    def right_arrow_button(self):
+        pass
+
+    def up_arrow_button(self):
+        pass
+
+    def down_arrow_button(self):
+        pass
+
+    def camera_start(self):
+        pass
+
+    def camera_stop(self):
+        pass
+
+    def camera_continue(self):
+        pass
+
+    def paint_camera_grid(self):
+        pass
+
+    def paint_camera_star(self):
+        pass
+
+    def paint_camera_trajectory(self):
+        pass
+
+    #як це можна винести в окремий потік?
+    @Slot(QTimer)
+    def updateCurrentTime(self):
+        self.CurrentTimeValueEdit.setText(str(QTime.currentTime().toPython()))
+
     def load(self):
         axSelect = QAxSelect(self)
         if axSelect.exec() == QDialog.Accepted:
@@ -206,8 +365,8 @@ class MainWindow(QMainWindow):
         #line in_file.readLine()
 
 
-    def handleButton(self):
-        self.open()
+#    def handleButton(self):
+#        self.open()
 
     def Plan_CU(self):
         self.widget = Plan_CU()
@@ -238,19 +397,51 @@ class Control(QDialog):
         generalsettings = QAction("General", self, shortcut = "Ctrl+G", triggered=self.setgeneralsettings)
         settingsMenu.addAction(generalsettings)
 
-        #buttons_layout = self.create_buttons_layout()
-        #lists_layout = self.create_lists_layout()
+        buttons_layout = self.create_buttons_layout()
+        lists_layout = self.create_lists_layout()
 
         self.main_layout = QGridLayout(self)
-        #self.main_layout.addLayout(buttons_layout,0,0,4,20)
-        #self.main_layout.addLayout(lists_layout,4,0,15,20)
+        self.main_layout.addLayout(buttons_layout,0,0,2,20)
+        self.main_layout.addLayout(lists_layout,2,0,17,20)
         self.main_layout.addWidget(self.probarwidget._progress_bar,19,0,1,20)
+
+        self.setFixedSize(QSize(1000,1000))
         
     def create_buttons_layout(self):
-        pass
+        self.date_edit_line = QDateEdit()
+        init_widget(self.date_edit_line, "date_editDateTimeEdit")
+        self.date_edit_line.setDate(QDate.currentDate())
+
+        self.auto_select_button = QPushButton("Auto Select")
+        init_widget(self.auto_select_button, "auto_selectPushButton")
+
+        self.select_button = QPushButton("Select")
+        init_widget(self.select_button, "selectPushButton")
+
+        self.delete_button = QPushButton("Delete")
+        init_widget(self.delete_button, "deletePushButton")
+
+        local_layout = QHBoxLayout()
+        local_layout.addWidget(self.date_edit_line,10)
+        local_layout.addWidget(self.auto_select_button,10)
+        local_layout.addWidget(self.select_button,10)
+        local_layout.addWidget(self.delete_button,5)
+
+        return local_layout
 
     def create_lists_layout(self):
-        pass
+        self.list_fcu = QListWidget()
+        init_widget(self.list_fcu, "list_fcu_ListWidget")
+
+        self.table_list = QTableWidget()
+        init_widget(self.table_list, "table_list_TableWidget")
+
+        local_layout = QHBoxLayout()
+        local_layout.addWidget(self.list_fcu,20)
+        local_layout.addStretch(1)
+        local_layout.addWidget(self.table_list,15)
+        
+        return local_layout
 
     def open_fcu_file(self):
         file_name = open_file(self)
@@ -495,8 +686,13 @@ class Plan_CU(QDialog):
         self.edit_text_1.setValue(1)          
         #отримує посилання на файл обраний через вікно treeview
         #print(self.getFullPath())
-    
-
+        
+        msgbx = QMessageBox(self)
+        msgbx.setText("Done!")
+        msgbx.setWindowTitle("Processing...")
+        #msgbx.setFixedSize(QSize(200,100))
+        msgbx.show()
+        #QMessageBox.done(self, 0)
 
 
     
