b16a4aa6f02308a59ae95921333b3a56cdb8468d
lrikozavr <sorokakiril@gmail.com>
2024-12-03 01:59:50 +0200

    upd

--- image_/support.py
+++ image_/support.py
@@ -277,3 +277,169 @@ def hour_ra_(angle, siderial_time, local_longitude = 0):
     if(result < 0):
         result += np.pi*2
     return result
+
+
+######################################################################################
+
+# Функція яка обраховує зсув
+# Перше значення, це відстань
+# Друге значення, це варіація тангенсу кута у полярній системі координат
+shift = lambda xy_1, xy_2: [np.hypot(xy_1[0] - xy_2[0], xy_1[1] - xy_2[1]),np.arctan2((xy_1[0] - xy_2[0]),(xy_1[1] - xy_2[1]))]
+
+#  
+def step(ra: np.ndarray, array_ra: np.ndarray,accuracy: list,aperture: list):
+    """
+    Функція яка виводить індекси рядків масиву, які задовільняють умовам вікна ототожнення
+
+    Input
+    -----
+    ra : [radius, angle]
+    array_ra : array of [radius, angle]
+    accuracy : percent shift of [radius, angle] as radius of searching window
+    aperture : solid shift of [radius, angle] of searching window
+
+    Output
+    ------
+    1D array of index 
+    """
+    bool_temp = ((array_ra[:,0] < ra[0]*(1+accuracy[0]) + aperture[0]) & (array_ra[:,0] > ra[0]*(1-accuracy[0]) - aperture[0])) & ((array_ra[:,1] < ra[1]*(1+accuracy[1]) + aperture[1]) & (array_ra[:,1] > ra[1]*(1-accuracy[1]) - aperture[1]))
+    if(bool_temp.any()):
+        return np.argwhere(bool_temp == True).flatten()
+    else:
+        return -1
+
+def cycle(ra,pair,i,j,N,gap_value,acc,ap):
+    """
+    Рекурсивна функція пошуку відповідників
+
+    Input
+    -----
+    ra : [radius, angle]
+    pair : 3D array like (index of base array, index of sub array,[radius, angle])
+    i : index of base array
+    j : array of used index of xub array
+    N : number of unit (of base array), that must have oponent
+    gap_value : number of unit (of base array) that may not have oponent
+    acc : (accuracy) percent of [radius, angle] as radius of searching window
+    ap : (aperture) solid [radius, angle] of searching window
+
+    Output
+    ------
+    if not have oponent => -1
+
+    else                => [index of oponents] 
+    """
+    
+    index = step(ra,pair[i],acc,ap)
+    
+    #якщо не вдалося знайти відповідник(-и) виводимо -1
+    if(not isinstance(index,np.ndarray)):
+        #Другий шанс
+        if((not i >= N-1) and (i - len(j) < gap_value)):
+            temp = cycle(ra,pair,i+1,j,N,gap_value,acc,ap)
+            if(isinstance(temp,np.ndarray)):
+                return np.append([-1],temp)
+                    
+        return -1
+
+    # якщо відповідники знайшлися, 
+    # але наступне коло виходить 
+    # за межі кількості відповідників N
+    # виводимо перший елемент
+    if(i == N-1):
+        #print(index)
+        for i_i in index:
+            if(not i_i in j):
+                return np.array(i_i)
+        return -1
+
+    indx = []
+    idx = -1
+    for i_i in index:
+        # якщо виникає повтор то переходимо до наступного індексу
+        if(i_i in j):
+            continue
+        # додаємо індекс до переліку вже використаних індексів
+        j_j = [i_i]
+        j_j.extend(j)
+        # j_j можна вважати набором індексів (це на потім)
+        #print(j_j)
+        # 1
+        # якщо індексів ототожнених рядків на цьому колі більше ніж 1
+        # і ми маємо можливість запустити позанаступне коло
+        # то ви запускаємо коло i + 1
+        if(isinstance(indx,np.ndarray) and not i >= N-2):
+            j_j.extend(indx)
+            temp = cycle(ra,pair,i+2,j_j,N,gap_value,acc,ap)
+            #мом
+            if(not isinstance(temp,np.ndarray)):
+                return -1
+            indx = np.append(indx,temp)
+            idx = i_i
+            break
+        # запускає пошук відповідників на наступне коло 
+        # збільшуючи і на 1
+        indx = cycle(ra,pair,i+1,j_j,N,gap_value,acc,ap)
+        # якщо невдалося знайти відповідник
+        if(not isinstance(indx,np.ndarray)):
+            # 1
+            # пробуємо перевірити гіпотезу, 
+            # що на наступному колі є відповідник 
+            # під індексом який ми вже зайняли на цьому колі 
+            indx = step(ra,np.array([pair[i+1][i_i]]),acc,ap)
+            if(not isinstance(indx,np.ndarray)):
+                return -1
+            #continue
+            # 1
+            # якщо нам вдається, то ми кладемо в indx значення індексу цього рядку
+            else:
+                indx = np.array([i_i])
+        else:
+            idx = i_i
+            break
+
+    if(idx == -1):
+        return -1
+
+    return np.append([idx],indx)
+
+def one(xy_1,xy_2, N, gap_value = 0, accuracy = [0.1,0.1], aperture=[0,0]):
+    """
+    Алгоритм пошуку відповідників за лінійного значення зсуву
+
+    Input
+    -----
+    xy_1 : base coordinate array
+    xy_2 :  sub coordinate array
+    N : number of unit (of base array), that must have oponent
+    gap_value : number of unit (of base array) that may not have oponent
+    acc : (accuracy) percent of [radius, angle] as radius of searching window
+    ap : (aperture) solid [radius, angle] of searching window
+
+    Output
+    ------
+       idx - np.array() of index from sub array
+    """
+    n = len(xy_2)
+    
+    # розраховуємо відстані одразу для N перших точок точок базового масиву
+    pair = np.zeros((N,n,2))
+    for i in range(0,N,1):
+        for j in range(0,n,1):
+            pair[i,j] = shift(xy_1[i],xy_2[j])
+    
+    # запускаємо рекурсію відштовхуючись від значення відстані для першої точки
+    idx = []
+    for j in range(0,n,1):    
+        # point 2
+        temp_idx = cycle(pair[0,j],pair,1,[j],N,gap_value,accuracy,aperture)
+        # у випадку невдалого пошуку, беремо наступне значення відстані
+        if (not isinstance(temp_idx,np.ndarray)):
+            continue
+        idx = np.append([np.int64(j)],temp_idx)
+        break
+    # якщо алгоритм так і не знайшов відповідників, то пишемо
+    if(not isinstance(idx,np.ndarray)):
+        print("Something go wrong. Increse accuracy")
+    # і виводимо -1
+    return idx
